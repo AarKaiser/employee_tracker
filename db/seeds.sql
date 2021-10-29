1ad06d26986b98b28cf4982263d658e238b9c45b
Aar Kaiser <dev@aarkaiser>
2021-10-29 03:04:07 -0400

    added quotes to department names to fix bug

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,7 @@
 insert into departments (id, name)
-values  (1, Strategy),
-        (2, Markeing),
-        (3, Finance),
-        (4, Human Resources),
-        (5, Technology & Equipment),
-        (6, Operations);
-
-
-
-
+values  (1, "Strategy"),
+        (2, "Markeing"),
+        (3, "Finance"),
+        (4, "Human Resources"),
+        (5, "Technology & Equipment"),
+        (6, "Operations");
