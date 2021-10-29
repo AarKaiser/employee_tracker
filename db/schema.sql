3a23d6fe60aafaa821729a83128b80c8fa794360
Aar Kaiser <dev@aarkaiser>
2021-10-29 02:48:33 -0400

    updating seeds with depts

--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,20 @@
+-- Droping & Creating Database
+
 drop database if exists employee_db;
 create database employee_db;
+
+-- Specifying Database to Use
 use employee_db;
+
+-- Creating Departments Table
 
 create table departments (
     id int not null auto_increment primary key,
     name varchar(50)
 );
+
+-- Creating Roles Table
+create table roles (
+    id int not null auto_increment primary key,
+
+)
