46c5878f65bd6d97d93796f778cc8a3bac909b2e
Aar Kaiser <dev@aarkaiser>
2021-10-29 02:24:17 -0400

    updating comments

--- utils/departments.js
+++ utils/departments.js
@@ -1,9 +1,15 @@
+// Requiring Server Connection
+
 const db = require("../config/connection")
+
+// Function for Viewing Departments
 
 function viewDepts() {
   console.log("\x1b[32m", "view dept working");
-  
+
 }
+
+// Function for Adding Department
 
 function addDept(){
     console.log ("\x1b[32m", "add dept working")
