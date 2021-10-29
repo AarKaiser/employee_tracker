252c2827511c9ed303f17d19d05ac2391fc6f079
Aar Kaiser <dev@aarkaiser>
2021-10-29 02:07:03 -0400

    converted if to switch

---
+++ utils/departments.js
@@ -0,0 +1,9 @@
+function viewDepts() {
+  console.log("view dept working");
+}
+
+function addDept(){
+    console.log ("add dept working")
+}
+
+module.exports = { viewDepts, addDept };
--- utils/main_menu.js
+++ utils/main_menu.js
@@ -1,13 +1,13 @@
 const inquirer = require("inquirer");
 const {viewDepts, addDept} = require("./departments");
 const {viewEmployees, addEmployee, updateEmployee} = require("./employees");
-const {viewRole, addRole} = require("./roles")
+const {viewRoles, addRole} = require("./roles")
 
 const questions = [
   {
     type: "list",
     message: "What would you like to do?",
-    name: "menuchoice",
+    name: "menuchoices",
     choices: [
       "View All Departments",
       "View All Roles",
@@ -23,14 +23,34 @@ const questions = [
 
 function mainMenu() {
 //   console.log("\x1b[32m", "\nSucessfully accessed Main Menu √");
-  inquirer.prompt(questions).then((answers) => {
-    if (answers.choices === "View All Departments") {
-        
-    } else if (answers.choices === "View All Roles") {
-        console.log("view roles");
-    } else {
+  inquirer.prompt(questions).then((answer) => {
+  const navigate = answer.choices;
+  switch (navigate) {
+    case "View All Departments":
+        console.log("view all dept selected");
+        viewDepts();
+        break;
+    case "View All Roles":
+        viewRoles();
+        break;
+    case "View All Employees":
+        viewEmployees();
+        break;
+    case "Add a Department":
+        addDept();
+        break;
+    case "Add a Role":
+        addRole();
+        break
+    case "Add an Employee":
+        addEmployee();
+        break;
+    case "Update An Employee Role":
+        updateEmployee();
+        break;
+    default: 
         process.exit();
-    }
+  }
     
   });
  
