4d995adfa2a06d4959a26c76db547a386daeef35
Aar Kaiser <dev@aarkaiser>
2021-11-05 01:02:24 -0400

    updating tonight's work

--- db/seeds.sql
+++ db/seeds.sql
@@ -19,13 +19,13 @@ values  (1,"Chief Strategist", 1,150000),
         (10,"Head of Legal", 6, 200000);
 
 insert into employees (id, first_name, last_name,role_id, manager_id)
-values(1,"Masai","Ujiri",1,null),
-(2,"Nick","Nurse",2,1),
-(3,"Scottie","Barnes",3,null),
-(4,"Pascal","Siakam",4,3),
-(5,"Gary","Trent Jr.",5, null),
-(6,"OG","Anunoby",6,5),
-(7,"Fred","VanVleet",7,null),
-(8,"Chris","Boucer",8,7);
-(9,"Sviatoslav","Makhailiuk",8,7);
-(10,"Dalano","Banton",8,7);
+values  (1,"Masai","Ujiri",1,null),
+        (2,"Nick","Nurse",2,1),
+        (3,"Scottie","Barnes",3,2),
+        (4,"Pascal","Siakam",4,2),
+        (5,"Gary","Trent Jr.",5,2),
+        (6,"OG","Anunoby",6,2),
+        (7,"Fred","VanVleet",7,2),
+        (8,"Chris","Boucer",8,2),
+        (9,"Sviatoslav","Makhailiuk",9,2),
+        (10,"Dalano","Banton",10,2);
