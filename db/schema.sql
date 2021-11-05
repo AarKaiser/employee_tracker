9bb53bd047d0f1839c9b0001da45209b8513f138
Aar Kaiser <dev@aarkaiser>
2021-11-05 10:06:36 -0400

    updating fixe seeds

--- db/schema.sql
+++ db/schema.sql
@@ -8,7 +8,7 @@ use employee_db;
 
 -- Creating Departments Table
 
-create table departments (
+create table departments(
     id int not null auto_increment primary key,
     name varchar(50)
 );
@@ -18,7 +18,7 @@ create table departments (
 create table roles (
     id int auto_increment primary key,
     title varchar(50) not null,
-    salary not null decimal,
+    salary decimal not null,
     dept_id int,
         foreign key (dept_id)
         references departments(id)
@@ -31,7 +31,7 @@ create table employees (
     first_name varchar(50) not null,
     last_name varchar(50) not null,
     role_id int,
-    manager_id INT,
+    manager_id int null,
         foreign key (role_id)
         references roles(id)
         on delete set null,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,31 +1,33 @@
-insert into departments (id, name)
-values  (1, "Strategy"),
-        (2, "Markeing"),
-        (3, "Finance"),
-        (4, "Human Resources"),
-        (5, "Technology & Equipment"),
-        (6, "Operations");
+insert into departments (name)
+values  ("Strategy"),
+        ("Markeing"),
+        ("Finance"),
+        ("Human Resources"),
+        ("Technology & Equipment"),
+        ("Operations");
 
-insert into roles (role_id,title dept_id,title, dept_id, salary)
-values  (1,"Chief Strategist", 1,150000),
-        (2,"Head of Marketing", 2, 125000),
-        (3,"Head of Finance", 3, 200000),
-        (4,"Head of Human Resources", 4, 80000),
-        (5,"Chief Engineer", 130000),
-        (6,"Senior Developer", 5,175000),
-        (7,"CEO", 6, 300000),
-        (8,"CTO",6, 250000),
-        (9,"CIO", 6, 220000),
-        (10,"Head of Legal", 6, 200000);
+insert into roles (title,salary,dept_id)
+values  ("Chief Strategist",150000,1),
+        ("Head of Marketing",125000,2),
+        ("Head of Finance",200000,3),
+        ("Head of Human Resources",80000,4),
+        ("Chief Engineer",130000,2),
+        ("Senior Developer",175000,5),
+        ("CEO", 300000,1),
+        ("CTO", 250000,2),
+        ("CIO", 220000,3),
+        ("Head of Legal", 200000,6);
 
-insert into employees (id, first_name, last_name,role_id, manager_id)
-values  (01,"Masai","Ujiri",1,null),
-        (02,"Nick","Nurse",2,1),
-        (03,"Scottie","Barnes",3,2),
-        (04,"Pascal","Siakam",4,2),
-        (05,"Gary","Trent Jr.",5,2),
-        (06,"OG","Anunoby",6,2),
-        (07,"Fred","VanVleet",7,2),
-        (08,"Chris","Boucer",8,2),
-        (09,"Sviatoslav","Makhailiuk",9,2),
-        (10,"Dalano","Banton",10,2);
+insert into employees (first_name,last_name,role_id,manager_id)
+values  ("Masai","Ujiri",1,null),
+        ("Nick","Nurse",2,1),
+        ("Scottie","Barnes",3,2),
+        ("Pascal","Siakam",4,2),
+        ("Gary","Trent Jr.",5,2),
+        ("OG","Anunoby",6,2),
+        ("Fred","VanVleet",7,2),
+        ("Chris","Boucer",8,2),
+        ("Sviatoslav","Makhailiuk",9,2),
+        ("Dalano","Banton",10,2);
+        
+select * from departments;
