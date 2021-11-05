bc19a15948e51d734b9907145e08f3aa5cfe18f3
Aar Kaiser <dev@aarkaiser>
2021-11-05 00:55:21 -0400

    updating tonight's work

--- utils/employees.js
+++ utils/employees.js
@@ -9,7 +9,7 @@ function viewEmployees() {
 }
 
 function addEmployee() {
-    console.log("\x1b[32m", "view employees working √");
+    console.log("\x1b[32m", "add employees working √");
     let query = "select distinct name from employee_db.employees";
     return db.promise().query(query);
   }
