209ebe8f79543bd8da1c3837aa3ce4b6c96e38d8
Aar Kaiser <dev@aarkaiser>
2021-11-09 21:58:27 -0500

    updating for bcs

--- utils/roles.js
+++ utils/roles.js
@@ -1,36 +1,42 @@
 const { db } = require("../config/connection");
-const inquirer = require("inquirer")
+const inquirer = require("inquirer");
 let departmentArray = [];
-let rolesArray =[];
+let employeeArray = [];
+let roleArray = [];
 
 function viewRoles() {
-    console.log("\x1b[32m", "view role working √");
-    let query = "select title from employee_db.roles";
-    return db.promise().query(query);
-  }
+  console.log("\x1b[32m", "view role working √");
+  let query = "select title from employee_db.roles";
+  return db.promise().query(query);
+}
 
 function loadDepartments() {
-  let query = `SELECT * FROM departments`
-  return db.promise().query(query)
+  let query = `select * from departments`;
+  return db.promise().query(query);
 }
 
 function loadEmployees() {
-  let query = `SELECT * FROM employees`
-  return db.promise().query(query)
+  let query = "select first_name from employee_db.employees";
+  return db.promise().query(query);
 }
 
-  async function addRole() {
-    let sqlData = await loadDepartments()
-    let stringifiedData = JSON.stringify(sqlData[0])
-    for(i=0; i< JSON.parse(stringifiedData).length; i++) {
-      const newObj = {
-        name: JSON.parse(stringifiedData)[i].name,
-        value: JSON.parse(stringifiedData)[i].id
-      }
-      departmentArray.push(newObj)
-    }
-    console.log("\x1b[32m", "add role working √");
-    return inquirer
+function loadRoles() {
+  let query = `select title from employee_db.roles`;
+  return db.promise().query(query);
+}
+
+async function addRole() {
+  let sqlData = await loadDepartments();
+  let stringifiedData = JSON.stringify(sqlData[0]);
+  for (i = 0; i < JSON.parse(stringifiedData).length; i++) {
+    let newObj = {
+      name: JSON.parse(stringifiedData)[i].name,
+      value: JSON.parse(stringifiedData)[i].id,
+    };
+    departmentArray.push(newObj);
+  }
+  console.log("\x1b[32m", "add role working √");
+  return inquirer
     .prompt([
       {
         type: "input",
@@ -46,38 +52,67 @@ function loadEmployees() {
         type: "list",
         name: "departmentId",
         message: "Which department does this belong to?",
-        choices: departmentArray
+        choices: departmentArray,
       },
     ])
     .then((answer) => {
-      console.log(answer)
+      console.log(answer);
       const sql = `
                   insert into roles (title, salary, dept_id)
                   values ("${answer.newTitle}","${answer.newSalary}","${answer.departmentId}")
                   `;
-      db.promise().query(sql)
-    })
-  }
+      db.promise().query(sql);
+    });
+}
 
-  async function updateRole() {
-    let sqlData = await loadEmployees()
-    let stringifiedData = JSON.stringify(sqlData[0])
-    for(i=0; i< JSON.parse(stringifiedData).length; i++) {
-      const newObj = {
-        name: JSON.parse(stringifiedData)[i].name,
-        value: JSON.parse(stringifiedData)[i].id
-      }
-      departmentArray.push(newObj)
-    }
-    console.log("\x1b[32m", "update role working √");
-    return inquirer
+async function updateRole() {
+  let sqlData = await loadEmployees();
+  let sqlData2 = await loadRoles();
+  // console.log(sqlData);
+  // console.log(sqlData2);
+  let stringifiedData = JSON.stringify(sqlData[0]);
+  let stringifiedData2 = JSON.stringify(sqlData2[0]);
+  console.log(stringifiedData);
+  console.log(stringifiedData2);
+  for (i = 0; i < JSON.parse(stringifiedData).length; i++) {
+    let newObj = {
+      first_name: JSON.parse(stringifiedData)[i].first_name,
+      value: JSON.parse(stringifiedData)[i].id,
+    };
+    employeeArray.push(newObj);
+  }
+  for (i = 0; i < JSON.parse(stringifiedData2).length; i++) {
+    let newObj2 = {
+      title: JSON.parse(stringifiedData2)[i].title,
+      value: JSON.parse(stringifiedData2)[i].id,
+    };
+    roleArray.push(newObj2);
+  }
+  console.log("\x1b[32m", "update role working √");
+  return inquirer
     .prompt([
       {
         type: "list",
-        name: "newTitle",
-        message: "Which role would you like to update?",
+        name: "updateEmpl",
+        message: "Which employee would you like to update?",
+        choices: employeeArray,
       },
-  }
+      {
+        type: "list",
+        name: "updateRole",
+        message: "What is the employees new role?",
+        choices: roleArray,
+      },
+    ])
+    .then((answer) => {
+      console.log(answer);
+      // const sql = `
+      //             insert into roles (title, salary, dept_id)
+      //             values ("${answer.newTitle}","${answer.newSalary}","${answer.departmentId}")
+      //             `;
 
+      // db.promise().query(sql);
+    });
+}
 
-  module.exports = { viewRoles, addRole, updateRole };
+module.exports = { viewRoles, addRole, updateRole };
